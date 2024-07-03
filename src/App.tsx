4ce593bac5b7cf4a80719a1914f912fa96fe9a69
Domenico Gemoli <domenicogemoli@gmail.com>
2024-07-03 13:55:33 +0200

    Add DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary mt-4">DaisyUI example Button</button>
     </div>
   );
 }
