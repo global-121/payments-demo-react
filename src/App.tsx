49cf2bbc64a8695244b6fce5532e3d58fc7093cd
Domenico Gemoli <domenicogemoli@gmail.com>
2024-07-03 14:08:31 +0200

    Add basic 510 theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-100 min-h-screen font-body">
+      <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button className="btn btn-primary mt-4">DaisyUI example Button</button>
     </div>
